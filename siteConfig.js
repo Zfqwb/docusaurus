bdcd41d05b30057b509cfeedf169a7241dab46c4
nitto <3468531136@qq.com>
2021-06-29 09:49:35 +0800

    Update siteConfig.js

--- siteConfig.js
+++ siteConfig.js
@@ -35,7 +35,7 @@ const siteConfig = {
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'test-site',
+  projectName: 'tantan',
   organizationName: 'facebook',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
